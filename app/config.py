92da942bccb2d6fe257f54ef0337cad837b42026
aki. <mark@aur.ll.to>
2025-05-12 21:28:30 +0900

    .

--- app/config.py
+++ app/config.py
@@ -23,6 +23,7 @@ FLASK_CONFIG = {
     "SECRET_KEY": pytilpack.secrets_.generate_secret_key(DATA_DIR / ".secret_key"),
     "SESSION_COOKIE_NAME": "gla-session",
     "SESSION_COOKIE_HTTPONLY": True,
+    "SESSION_COOKIE_SECURE": True,
     "PERMANENT_SESSION_LIFETIME": datetime.timedelta(days=365),
     "SEND_FILE_MAX_AGE_DEFAULT": 12 * 3600,  # Flask 2.0対策
     "DEBUG": True,
@@ -30,6 +31,5 @@ FLASK_CONFIG = {
     # Quart-Auth
     # https://quart-auth.readthedocs.io/en/latest/how_to_guides/configuration.html
     "QUART_AUTH_COOKIE_NAME": "gla-remember",
-    "QUART_AUTH_COOKIE_SECURE": False,
     "QUART_AUTH_SALT": "gla-auth",
 }
